641a0eb3826bbca0dea97b8424ffabbc084251c0
shiryauwu <70473372+shiryauwu@users.noreply.github.com>
2023-04-05 12:37:00 +0300

    Delete Mango.Services.Identity directory

--- Mango.Services.Identity/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Mango.Services.Identity.Models
-{
-    public class ApplicationUser: IdentityUser
-    {
-
-    }
-}
--- Mango.Services.Identity/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mango.Services.Identity.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
